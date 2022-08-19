3c91b0367e1d83c1cf73b6af2fb40f290a7e83b7
ilya khlevnoy <71222850+Plyha@users.noreply.github.com>
2022-08-19 12:35:04 +0300

    Add consumer and update readme

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Самый простой клиент для отправки сообщений с использованием SpringBoot и брокера сообщений Kafka <br/>
 
 Как настроить среду: <br/>
-1.Переместить папку kafka_2.13-3.2.1 в корень диска C:\ <br/>
+1.Распаковать архив kafka_2.13-3.2.1 в корень диска C:\ <br/>
 
 2.Запустить Zookeper.В CMD ввести ```bash C:\kafka_2.13-3.2.1\bin\windows\zookeeper-server-start.bat C:\kafka_2.13-3.2.1\config\zookeeper.properties ``` <br/>
 
@@ -13,4 +13,6 @@
 
 5.Запустить Consumer.В CMD ввести ```bash  C:\kafka_2.13-3.2.1\bin\windows\kafka-console-consumer.bat --bootstrap-server localhost:9092 --topic stirng_java --from-beginning ``` <br/>
 
-6.Ввести данный текст в браузере/Postman http://localhost:8080/kafka/producer?message=test <br/>
+6.Ввести данный текст в браузере/Postman, где message произвольное сообщение, к примеру test http://localhost:8080/kafka/producer?message=test <br/>
+
+7.В корне проекта появится папка "SavedMessagesFolder" куда будут складываться все сообщения. <br/>
---
+++ src/main/java/plyha/com/demo/consumer/Consumer.java
@@ -0,0 +1,49 @@
+package plyha.com.demo.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+public class Consumer {
+
+    private final Path dir = Paths.get(".\\SavedMessagesFolder");
+
+    @KafkaListener(topics = {"String_Kafka"})
+    public void listen(ConsumerRecord<?, ?> record) {
+
+        System.out.printf("topic = %s, offest = %d, value = %s\n",record.topic(),record.offset(),record.value());
+        createDir();
+        saveMessage(record);
+
+    }
+    private void saveMessage(ConsumerRecord<?, ?> record){
+
+        String fileName = record.topic()+record.offset()+".txt";
+        try(FileWriter fw = new FileWriter(dir+ File.separator + fileName,false)) {
+            String text = record.value().toString();
+            fw.write(text);
+            System.out.printf("The file %s was saved in root directory", fileName);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void createDir(){
+
+        if(!Files.exists(dir)){
+            try {
+                Files.createDirectories(dir);
+            }catch (IOException e){
+                System.out.printf("Can't create the directory %s",dir);
+            }
+        }
+    }
+}
